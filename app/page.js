fc39d01297a1d1614afb06a1e6ce1cca400d00b8
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2024-06-24 02:02:38 +0530

    To do list using react

--- app/page.js
+++ app/page.js
@@ -47,7 +47,7 @@ const page = () => {
       <form onSubmit={submitHandler}>
         <input
           type='text'
-            className='text-xl sm:text-2xl border-zinc-800 border-2 m-5 px-4 py-2 sm:w-2/3'
+            className='text-xl sm:text-xl border-zinc-800 border-2 m-5 px-4 py-2 sm:w-2/3'
           placeholder='Enter Task here'
           value={title}
           onChange={(e) => {
@@ -56,7 +56,7 @@ const page = () => {
         />
         <input
           type='text'
-          className='text-xl sm:text-2xl border-zinc-800 border-2 m-5 px-4 py-2 sm:w-2/3'
+          className='text-xl sm:text-xl border-zinc-800 border-2 m-5 px-4 py-2 sm:w-2/3'
           placeholder='Enter Description  here'
           value={desc}
           onChange={(e) => {
