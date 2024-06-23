9555e4b8670154fd0c66d6681ada18f891a99ca1
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2024-06-24 01:54:45 +0530

    To do list using react

--- app/page.js
+++ app/page.js
@@ -24,8 +24,8 @@ const page = () => {
   if (mainTask.length > 0) {
     renderTask = mainTask.map((t, i) => {
       return (
-        <li key={i} className='flex flex-col sm:flex-row items-center justify-between mb-8'>
-          <div className='flex flex-col sm:flex-row justify-between mb-5 w-full'>
+        <li key={i} className='flex items-center justify-between mb-8'>
+          <div className='flex flex-col sm:flex-row justify-between mb-5 w-2/3'>
             <h5 className='text-2xl font-semibold'>{t.title}</h5>
             <h6 className='text-lg font-medium'>{t.desc}</h6>
           </div>
@@ -33,27 +33,30 @@ const page = () => {
             onClick={() => {
               deleteHandler(i)
             }}
-            className='bg-red-400 text-white px-4 py-2 rounded font-bold'>Delete</button>
+            className='bg-red-400 text-white px-4 py-2 rounded font-bold'
+          >
+            Delete
+          </button>
         </li>
-      );
+      )
     })
   }
   return (
     <>
       <h1 className='bg-black text-white p-5 text-5xl font-bold text-center'>My Todo List</h1>
       <form onSubmit={submitHandler}>
-        <input type='text'
-          className='text-2xl border-zinc-800 border-2 m-5
-      px-4 py-2'
+        <input
+          type='text'
+          className='text-2xl border-zinc-800 border-2 m-5 px-4 py-2'
           placeholder='Enter Task here'
           value={title}
           onChange={(e) => {
             settitle(e.target.value)
           }}
         />
-        <input type='text'
-          className='text-2xl border-zinc-800 border-2 m-5
-      px-4 py-2'
+        <input
+          type='text'
+          className='text-2xl border-zinc-800 border-2 m-5 px-4 py-2'
           placeholder='Enter Description  here'
           value={desc}
           onChange={(e) => {
@@ -63,7 +66,7 @@ const page = () => {
         <button className='bg-black text-white px-4 py-3 text-2xl font-bold rounded m-5'>Add Task</button>
       </form>
       <hr />
-      <div className='w-full sm:w-full p-8 bg-slate-300'>
+      <div className='w-full sm:w-f p-8 bg-slate-300'>
         <ul>{renderTask}</ul>
       </div>
     </>
