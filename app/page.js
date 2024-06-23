c32f2c45decce2523477c5a8891dff9bdb4ee9bd
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2024-06-24 01:47:04 +0530

    To do list using react

--- app/page.js
+++ app/page.js
@@ -26,26 +26,25 @@ const page = () => {
       return (
         <li key={i} className='flex items-center justify-between mb-8'>
           <div className='flex justify-between mb-5 w-2/3'>
-          <h5 className='text:xl sm:text-2xl font-semibold'>{t.title}</h5>
-          <h6 className='text:xl sm:text-2xl font-medium'>{t.desc}</h6>
+          <h5 className='text-2xl font-semibold'>{t.title}</h5>
+          <h6 className='text-lg font-medium'>{t.desc}</h6>
         </div>
         <button
         onClick={()=>{
           deleteHandler(i)
         }} 
-        className='bg-red-400 text-white px-2 py-1 mb-5 ml-2 sm:px-4 sm:py-2 rounded font-bold'>Delete</button>
+        className='bg-red-400 text-white px-4 py-2 rounded font-bold'>Delete</button>
         </li>
       );
     })
   }
   return (
     <>
-    <h1 className='bg-black text-white p-5 text-5xl font-bold text-center 
-    sm:w-full w-full'>My Todo List</h1>
+    <h1 className='bg-black text-white p-5 text-5xl font-bold text-center'>My Todo List</h1>
     <form onSubmit={submitHandler}>
       <input type='text' 
-      className='text-xl sm:text-2xl border-zinc-800 border-2 m-5 
-      px-4 py-2 sm:w-auto w-auto'
+      className='text-2xl border-zinc-800 border-2 m-5 
+      px-4 py-2'
       placeholder='Enter Task here'
       value={title}
       onChange={(e)=>{
@@ -53,8 +52,8 @@ const page = () => {
       }}
       />
       <input type='text' 
-      className='text-xl sm:text-2xl border-zinc-800 border-2 m-5 
-      px-4 py-2 w-21 sm:w-auto'
+      className='text-2xl border-zinc-800 border-2 m-5 
+      px-4 py-2'
       placeholder='Enter Description  here'
       value={desc}
       onChange={(e)=>{
@@ -64,7 +63,7 @@ const page = () => {
       <button className='bg-black text-white px-4 py-3 text-2xl font-bold rounded m-5'>Add Task</button>
     </form>
     <hr/>
-    <div className='w-full sm:w-full p-8 bg-slate-300'>
+    <div className='w-full sm:w-fit p-8 bg-slate-300'>
       <ul>{renderTask}</ul>
     </div>
     </>
